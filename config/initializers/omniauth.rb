7066346e207905a813faa5a8d191e979f8113389
Yoseph Anderson <yrganderson@gmail.com>
2016-09-17 13:43:23 -0600

    correct config

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :developer unless Rails.env.production?
-  provider :github, "d9b967a5bc5975ade778", "1168513829478dca030afec6fb6bb300e4cc9228"
-  # provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-# end
+  provider :developer unless Rails.env.production?
+  provider :github, ENV['CLIENT_ID'], ENV['CLIENT_SECRET']
+end
 end
